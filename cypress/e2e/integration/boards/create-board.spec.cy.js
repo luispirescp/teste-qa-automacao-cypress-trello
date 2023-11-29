58bf7a68f5bfe805271d70801912ad2c16e54168
luisp <luispires.m@gmail.com>
2023-11-29 12:38:16 -0300

    ajuste identação

--- cypress/e2e/integration/boards/create-board.spec.cy.js
+++ cypress/e2e/integration/boards/create-board.spec.cy.js
@@ -1,40 +1,40 @@
 describe('Teste cria board e captura idList', () => {
-  let   boardId="";
-it('Deve cadastrar um board no Trello e salvar o ID em um arquivo', () => {
-  const boardName = 'Novo board Cypress';
-  cy.request({
-    method: 'POST',
-    url: `${Cypress.env('apiUrl')}/boards/`,
-    body: {
-      name: boardName,
-      key: Cypress.env('trelloKey'),
-      token: Cypress.env('trelloToken')
-    }
-  }).then((response) => {
-    expect(response.status).to.equal(200);
-    expect(response.body.name).to.equal(boardName);
+  let boardId = "";
+  it('Deve cadastrar um board no Trello e salvar o ID em um arquivo', () => {
+    const boardName = 'Novo board Cypress';
+    cy.request({
+      method: 'POST',
+      url: `${Cypress.env('apiUrl')}/boards/`,
+      body: {
+        name: boardName,
+        key: Cypress.env('trelloKey'),
+        token: Cypress.env('trelloToken')
+      }
+    }).then((response) => {
+      expect(response.status).to.equal(200);
+      expect(response.body.name).to.equal(boardName);
 
-   boardId = response.body.id;
+      boardId = response.body.id;
 
-    // Salva o ID do board em um arquivo temporário
-    cy.writeFile('cypress/temp/boardId.txt', boardId);
+      // Salva o ID do board em um arquivo temporário
+      cy.writeFile('cypress/temp/boardId.txt', boardId);
+    });
   });
-});
 
-it('Captura o ID da lista e salva em um arquivo temporário', () => {
-  cy.request({
-    method: 'GET',
-    url: `${Cypress.env('apiUrlBoard')}${boardId}/lists?key=${Cypress.env('trelloKey')}&token=${Cypress.env('trelloToken')}`
-  }).then(response => {
-    expect(response.status).to.equal(200);
-    expect(response.body.length).to.be.greaterThan(0); // Verifica se há listas retornadas
+  it('Captura o ID da lista e salva em um arquivo temporário', () => {
+    cy.request({
+      method: 'GET',
+      url: `${Cypress.env('apiUrlBoard')}${boardId}/lists?key=${Cypress.env('trelloKey')}&token=${Cypress.env('trelloToken')}`
+    }).then(response => {
+      expect(response.status).to.equal(200);
+      expect(response.body.length).to.be.greaterThan(0); // Verifica se há listas retornadas
 
-    // Pega o ID da primeira lista (você pode ajustar isso dependendo do seu caso de uso)
-    const idList = response.body[0].id;
+      // Pega o ID da primeira lista (você pode ajustar isso dependendo do seu caso de uso)
+      const idList = response.body[0].id;
 
-    // Salva o ID da lista em um arquivo temporário
-    cy.writeFile('cypress/temp/idList.txt', idList);
-    cy.log(`ID da lista capturado: ${idList} e salvo em idList.txt`);
+      // Salva o ID da lista em um arquivo temporário
+      cy.writeFile('cypress/temp/idList.txt', idList);
+      cy.log(`ID da lista capturado: ${idList} e salvo em idList.txt`);
+    });
   });
-});
 });
--- cypress/e2e/integration/cards/create-card.spec.cy.js
+++ cypress/e2e/integration/cards/create-card.spec.cy.js
@@ -3,27 +3,27 @@ it('Deve criar um card e salvar o ID em um arquivo', () => {
   const cardName = 'Novo card Cypress';
   cy.readFile('cypress/temp/idList.txt').then((listId) => {
     if (listId) {
-  cy.request({
-    method: 'POST',
-    url: `${Cypress.env('apiUrl')}/cards`,
-    qs: {
-      idList: listId,
-      key: Cypress.env('trelloKey'),
-      token: Cypress.env('trelloToken')
-    },
-    body: {
-      name: cardName
+      cy.request({
+        method: 'POST',
+        url: `${Cypress.env('apiUrl')}/cards`,
+        qs: {
+          idList: listId,
+          key: Cypress.env('trelloKey'),
+          token: Cypress.env('trelloToken')
+        },
+        body: {
+          name: cardName
+        }
+      }).then((response) => {
+        expect(response.status).to.equal(200);
+
+        const cardId = response.body.id;
+
+        // Salva o ID do card em um arquivo temporário
+        cy.writeFile('cypress/temp/cardId.txt', cardId);
+      });
+    } else {
+      throw new Error('ID do board não está definido');
     }
-  }).then((response) => {
-    expect(response.status).to.equal(200);
-    
-    const cardId = response.body.id;
-    
-    // Salva o ID do card em um arquivo temporário
-    cy.writeFile('cypress/temp/cardId.txt', cardId);
   });
-} else {
-  throw new Error('ID do board não está definido');
-}
-});
 });
